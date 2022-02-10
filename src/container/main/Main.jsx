d830b4227786c10e8431af11eb265bb123d16477
Ebrima Sallah <sallahebrima@ymail.com>
2022-02-10 02:05:40 +0000

    main sass

---
+++ src/container/main/Main.jsx
@@ -0,0 +1,14 @@
+import "./Main.scss";
+import CardList from "../../component/cardlist/CardList";
+
+
+
+const Main= () => {
+  return (
+    <div className="main">
+      <CardList />
+    </div>
+  );
+}
+
+export default Main;
